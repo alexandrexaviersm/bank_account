3aa81c9e76392555116b256d8e765269d1b037aa
Alexandre Moreira Xavier de Sene <alexandrexaviersm@gmail.com>
2020-07-20 12:38:30 -0300

    Generates phoenix project

---
+++ config/config.exs
@@ -0,0 +1,31 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :bank_account,
+  ecto_repos: [BankAccount.Repo]
+
+# Configures the endpoint
+config :bank_account, BankAccountWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "3Z6/4LpOM7DV6+K707712nIV3Wf83U+a03D5l7eBviodfU8r9nX2sSnNG4kyp6rM",
+  render_errors: [view: BankAccountWeb.ErrorView, accepts: ~w(json), layout: false],
+  pubsub_server: BankAccount.PubSub,
+  live_view: [signing_salt: "0aH96L1X"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
