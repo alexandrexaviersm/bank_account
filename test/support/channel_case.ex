f72cce4f6391566b7b81a1fe7632f2e58ba35e9c
Alexandre Moreira Xavier de Sene <alexandrexaviersm@gmail.com>
2020-07-21 00:07:34 -0300

    Fix credo warning

--- test/support/channel_case.ex
+++ test/support/channel_case.ex
@@ -17,6 +17,8 @@ defmodule BankAccountWeb.ChannelCase do
 
   use ExUnit.CaseTemplate
 
+  alias Ecto.Adapters.SQL.Sandbox
+
   using do
     quote do
       # Import conveniences for testing with channels
@@ -29,10 +31,10 @@ defmodule BankAccountWeb.ChannelCase do
   end
 
   setup tags do
-    :ok = Ecto.Adapters.SQL.Sandbox.checkout(BankAccount.Repo)
+    :ok = Sandbox.checkout(BankAccount.Repo)
 
     unless tags[:async] do
-      Ecto.Adapters.SQL.Sandbox.mode(BankAccount.Repo, {:shared, self()})
+      Sandbox.mode(BankAccount.Repo, {:shared, self()})
     end
 
     :ok
