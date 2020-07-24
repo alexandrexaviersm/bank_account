d7a706c3e79e49f4e423c710a7a88b70ac45b450
Alexandre Moreira Xavier de Sene <alexandrexaviersm@gmail.com>
2020-07-24 02:21:37 -0300

    Create tests for Domain CreateCustomer

---
+++ lib/bank_account/create_customer.ex
@@ -0,0 +1,36 @@
+defmodule BankAccount.CreateCustomer do
+  @moduledoc """
+  Creating a new customer
+  """
+
+  alias BankAccount.Customer
+  alias BankAccount.UserEncryption.Security.Utils, as: UserEncryption
+
+  # verifica se já existe registro com o CPF informado
+  # If true
+  # # Atualiza o registro com as novas informações
+  # else
+  # # Cria um novo Customer
+  # # Cria um nova Nova Account com status pendent
+
+  # Verifica se todos os campos foram preenchidos
+  # If true
+  # # Atualiza o status da Account e cria um referral_code_to_be_shared
+  # # O sistema informa uma mensagem de sucesso, juntamente com o código
+  # # de 8 dígitos previamente criado
+  # else
+  # # O sistema informa uma mensagem de sucesso, retorna os dados fornecidos e o stautus pendent
+
+  ## Event-driven architectures
+  # # Publish Event: Customer Created
+  # # AccountOpening Subscribe Customer Created
+
+  def run(params) do
+    attrs = %{id: Ecto.UUID.generate(), unique_salt: UserEncryption.generate_unique_salt()}
+
+    merged_params = Map.merge(params, attrs)
+
+    %Customer{}
+    |> Customer.create_changeset(merged_params)
+  end
+end
