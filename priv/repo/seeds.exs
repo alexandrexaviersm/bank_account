41b603d6db509f99ae571eba9746c2319a116988
Alexandre Moreira Xavier de Sene <alexandrexaviersm@gmail.com>
2020-07-27 01:16:33 -0300

    Added seeds to start the application

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,25 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+import Ecto.Changeset
+
+alias BankAccount.Schema.{Account, Customer}
+
+customer_attrs = %{
+  referral_code: "xxxxxxxx",
+  cpf_hash: "cpf_hash",
+  encrypted_name: "base_user",
+  unique_salt: "xxxxxxxx"
+}
+
+{:ok, customer} =
+  %Customer{}
+  |> cast(customer_attrs, [:encrypted_name, :cpf_hash, :referral_code, :unique_salt])
+  |> BankAccount.Repo.insert()
+
+account_attrs = %{customer_id: customer.id, referral_code_to_be_shared: "12345678"}
+
+%Account{}
+|> cast(account_attrs, [:customer_id, :referral_code_to_be_shared])
+|> BankAccount.Repo.insert()
